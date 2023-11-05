6ad72d1f5e56292a2078b85279f9a30ecce48623
jimolina <israel.molina@gmail.com>
2023-11-05 08:47:11 -0500

    Testing api cache on Tournaments

--- app/api/tournament/route.js
+++ app/api/tournament/route.js
@@ -1,6 +1,8 @@
 import { connectToDB } from "@utils/database";
 import Tournament from "@models/tournament";
 
+export const dynamic = 'force-dynamic';
+
 export const GET = async ( req ) => {
     try {
         await connectToDB();
